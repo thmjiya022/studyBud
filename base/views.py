36910ec664c00d089459ea87c5921357d73cb52e
Thobani <mjiyakhothobani@gmail.com>
2024-03-17 01:23:56 +0200

    models

--- base/views.py
+++ base/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from . models import Room
 
-# Create your views here.
-rooms = [
-    {'id':1, 'name': "Let's learn python!"},
-    {'id':2, 'name': "Design with me"},
-    {'id':3, 'name': "Frontend developers"},
-]
+# # Create your views here.
+# rooms = [
+#     {'id':1, 'name': "Let's learn python!"},
+#     {'id':2, 'name': "Design with me"},
+#     {'id':3, 'name': "Frontend developers"},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
