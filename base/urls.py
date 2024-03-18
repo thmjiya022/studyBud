de2fc86b9b3026a143e15e4ec3fdc2d70a1e5b22
Thobani <mjiyakhothobani@gmail.com>
2024-03-18 23:05:33 +0200

    user-profile-page

--- base/urls.py
+++ base/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>', views.user_profile, name='user-profile'),
     
     path('create-room/', views.create_room, name = 'create-room'),
     path('update-room<str:pk>/', views.update_room, name = 'update-room'),
     path('delete-room<str:pk>/', views.delete_room, name = 'delete-room'),
     path('delete-message<str:pk>/', views.delete_message, name = 'delete-message'),
+   
 ]
